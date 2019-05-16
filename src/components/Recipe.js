10a35701e359e4e5d2a6a4774f4c4f02463e6c73
raytickb10 <raytickb10@students.ecu.edu>
2019-05-16 13:17:32 -0400

    Update Recipe.js

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Link } from "react-router-dom";
 
-const API_KEY = "Your-api-key";
+const API_KEY = "77a7004e5b982cb54e8e0b5031e70162";
 
 class Recipe extends React.Component {
   state = {
